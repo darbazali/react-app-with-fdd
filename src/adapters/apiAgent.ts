ec323791fc8857e6701fc4976d1d9e1030acb7a6
Interceptor <darbaz.me@gmail.com>
2024-08-29 15:15:02 +0300

    ``` feat: add apiAgent for Axios instance configuration
    - Created apiAgent in src/adapters/apiAgent.ts
    - Configured Axios instance with base URL, content type, and authorization headers

---
+++ src/adapters/apiAgent.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosInstance } from "axios";
+import { API_BASE_URL } from "../constants/envVariables";
+const TOKEN = "token_from_state";
+
+export const apiAgent: AxiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+    Accept: "application/json",
+    Authorization: `Bearer ${TOKEN}`,
+  },
+});

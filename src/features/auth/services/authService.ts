667e664ed61101da35641d0780d5083a4451dfbc
Interceptor <darbaz.me@gmail.com>
2024-09-01 09:22:07 +0300

    Remove semicolons to align with code style conventions in authService.ts

--- src/features/auth/services/authService.ts
+++ src/features/auth/services/authService.ts
@@ -1,23 +1,23 @@
-import { apiAgent } from "@adapters/apiAgent";
+import { apiAgent } from "@adapters/apiAgent"
 
 interface LoginParams {
-  username: string;
-  password: string;
+  username: string
+  password: string
 }
 
 interface IAuthService {
-  readonly login: (user: LoginParams) => Promise<void>;
-  readonly logout: () => Promise<void>;
+  readonly login: (user: LoginParams) => Promise<void>
+  readonly logout: () => Promise<void>
 }
 
 export class AuthService implements IAuthService {
   async login(user: LoginParams): Promise<void> {
     await apiAgent.post<LoginParams, { data: LoginParams }>("/login", {
       data: user,
-    });
+    })
   }
 
   async logout(): Promise<void> {
-    await apiAgent.post("/logout");
+    await apiAgent.post("/logout")
   }
 }

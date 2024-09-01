7ea33904e0bac7d1a549211ba58990e3053c839b
Interceptor <darbaz.me@gmail.com>
2024-09-01 10:23:05 +0300

    Add increment button to Counter component in Counter.tsx

--- src/features/counter/components/Counter.tsx
+++ src/features/counter/components/Counter.tsx
@@ -2,10 +2,13 @@ import { useCounterStore } from "../state/useCounterStore"
 
 const Counter = () => {
   const count = useCounterStore((state) => state.count)
+  const increment = useCounterStore((state) => state.increment)
 
   return (
     <div>
       <h1>Counter: {count}</h1>
+
+      <button onClick={increment}>Increment</button>
     </div>
   )
 }

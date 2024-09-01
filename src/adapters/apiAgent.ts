385221e7d24d61e8dfb708a5701c4c8503925e06
Interceptor <darbaz.me@gmail.com>
2024-09-01 13:07:55 +0300

    Update apiAgent to use token from useAuthStore state

--- src/adapters/apiAgent.ts
+++ src/adapters/apiAgent.ts
@@ -1,12 +1,14 @@
-import axios, { AxiosInstance } from "axios";
-import { API_BASE_URL } from "@constants/envVariables";
-const TOKEN = "token_from_state";
+import axios, { AxiosInstance } from "axios"
+import { API_BASE_URL } from "@constants/envVariables"
+import { useAuthStore } from "features/auth/state/useAuthStore"
+
+const token = useAuthStore.getState().token
 
 export const apiAgent: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     "Content-type": "application/json; charset=UTF-8",
     Accept: "application/json",
-    Authorization: `Bearer ${TOKEN}`,
+    Authorization: `Bearer ${token}`,
   },
-});
+})

02f02fb403c1ea15c8b8d60f2eb49d01fd66a703
Interceptor <darbaz.me@gmail.com>
2024-09-01 09:22:22 +0300

    Remove semicolons from User interface in TypeScript model

--- src/core/models/User.ts
+++ src/core/models/User.ts
@@ -1,8 +1,8 @@
 export interface User {
-  id: number;
-  displayName: string;
-  username: string;
-  status: string;
-  createAt: string;
-  token: string;
+  id: number
+  displayName: string
+  username: string
+  status: string
+  createAt: string
+  token: string
 }

80763e560ac19ebb30a3a94c00f39456db33518b
Interceptor <darbaz.me@gmail.com>
2024-09-01 10:22:58 +0300

    Add increment function to useCounterStore in state management

--- src/features/counter/state/useCounterStore.ts
+++ src/features/counter/state/useCounterStore.ts
@@ -2,8 +2,10 @@ import { create } from "zustand"
 
 interface CounterState {
   count: number
+  increment: () => void
 }
 
-export const useCounterStore = create<CounterState>(() => ({
+export const useCounterStore = create<CounterState>((set) => ({
   count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
 }))

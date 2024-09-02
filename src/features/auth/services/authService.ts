0d954dc7c5341f225f3eae3d503b6fc3078bc688
Interceptor <darbaz.me@gmail.com>
2024-09-02 15:25:44 +0300

    Export AuthService class for external usage

--- src/features/auth/services/authService.ts
+++ src/features/auth/services/authService.ts
@@ -3,7 +3,7 @@ import { LoginParams } from "../types"
 import AuthUserEntity from "@domain/entities/authEntity"
 import AuthUser from "@domain/models/AuthUser"
 
-class AuthService implements Exact<AuthService, AuthUserEntity> {
+export class AuthService implements Exact<AuthService, AuthUserEntity> {
   async login(user: LoginParams): Promise<AuthUser> {
     const { data } = await apiAgent.post<AuthUser>("/login", user)
     return data

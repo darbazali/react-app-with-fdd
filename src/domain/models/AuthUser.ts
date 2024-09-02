46dde1ad713f80d90b991e99c5c3eb0b4227cf1e
Interceptor <darbaz.me@gmail.com>
2024-09-02 13:27:06 +0300

    Add AuthUser interface to define user authentication model in TypeScript

---
+++ src/domain/models/AuthUser.ts
@@ -0,0 +1,13 @@
+interface AuthUser {
+  id: number
+  username: string
+  email: string
+  firstName: string
+  lastName: string
+  gender: string
+  image: string
+  token: string
+  refreshToken: string
+}
+
+export default AuthUser

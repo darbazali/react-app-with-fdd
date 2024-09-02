4583c5e85ff632fb252ff2e7c4b1757638c9df3a
Interceptor <darbaz.me@gmail.com>
2024-09-02 13:59:52 +0300

    Remove unused Exact type definition from authService.ts

--- src/features/auth/services/authService.ts
+++ src/features/auth/services/authService.ts
@@ -3,10 +3,6 @@ import { LoginParams } from "../types"
 import AuthUserEntity from "@domain/entities/authEntity"
 import AuthUser from "@domain/models/AuthUser"
 
-type Exact<T, U extends T> = T & {
-  [K in keyof U]: K extends keyof T ? U[K] : never
-}
-
 class AuthService implements Exact<AuthService, AuthUserEntity> {
   async login(user: LoginParams): Promise<AuthUser> {
     const { data } = await apiAgent.post<AuthUser>("/login", user)

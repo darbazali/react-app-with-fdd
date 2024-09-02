6c029946e5e8a2f767cfa1ecf6189b984d72e015
Interceptor <darbaz.me@gmail.com>
2024-09-02 13:54:13 +0300

    ```plaintext Refactor AuthService to use Exact type for stricter type checking

--- src/features/auth/services/authService.ts
+++ src/features/auth/services/authService.ts
@@ -3,7 +3,11 @@ import { LoginParams } from "../types"
 import AuthUserEntity from "@domain/entities/authEntity"
 import AuthUser from "@domain/models/AuthUser"
 
-class AuthService implements AuthUserEntity {
+type Exact<T, U extends T> = T & {
+  [K in keyof U]: K extends keyof T ? U[K] : never
+}
+
+class AuthService implements Exact<AuthService, AuthUserEntity> {
   async login(user: LoginParams): Promise<AuthUser> {
     const { data } = await apiAgent.post<AuthUser>("/login", user)
     return data

b3380ed448d3090dfe08aaa618be8cd05ef0aa42
Interceptor <darbaz.me@gmail.com>
2024-09-03 15:05:12 +0300

    Add user routes to main router configuration in routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom"
 import Counter from "features/counter/components/Counter"
+import userRoutes from "features/users/userRoutes"
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Counter />,
   },
+  userRoutes,
 ])

9b8b163383f88afdf8a5dc81b811abdff97966da
Interceptor <darbaz.me@gmail.com>
2024-09-03 15:24:31 +0300

    Refactor `getUsers` method to return `GetUsers` interface in `UserEntity`

--- src/domain/entities/userEntity.ts
+++ src/domain/entities/userEntity.ts
@@ -1,7 +1,14 @@
 import { User } from "@domain/models/User"
 
+export interface GetUsers {
+  limit: number
+  skip: number
+  total: number
+  users: User[]
+}
+
 interface UserEntity {
-  getUsers(): Promise<User[]>
+  getUsers(): Promise<GetUsers>
 }
 
 export default UserEntity

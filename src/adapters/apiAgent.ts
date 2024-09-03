9196030f9f4b4c0f30e60e2ff00989ab017ffce7
Interceptor <darbaz.me@gmail.com>
2024-09-03 15:24:17 +0300

    Remove token authorization from apiAgent configuration

--- src/adapters/apiAgent.ts
+++ src/adapters/apiAgent.ts
@@ -1,15 +1,11 @@
 import axios, { AxiosInstance } from "axios"
 import { API_BASE_URL } from "@constants/envVariables"
-import { useAuthStore } from "features/auth/state/useAuthStore"
 
 const apiAgent: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true,
 })
 
 apiAgent.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token
-  config.headers.Authorization = `Bearer ${token}`
   config.headers.Accept = "application/json"
   config.headers["Content-Type"] = "application/json; charset=UTF-8"
   return config

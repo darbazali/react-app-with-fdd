9edf778cd353fbf7f1bccdeb2f7f05e1b4696844
Interceptor <darbaz.me@gmail.com>
2024-09-10 15:15:59 +0300

    Refactor `useGetUsers` hook to use SWR for data fetching

--- src/features/users/hooks/useGetUsers.ts
+++ src/features/users/hooks/useGetUsers.ts
@@ -1,32 +1,15 @@
-import { useEffect, useState } from "react"
 import { userService } from "../services/userService"
 import { GetUsers } from "@domain/entities/userEntity"
+import useSWR from "swr"
 
 const useGetUsers = () => {
-  const [data, setData] = useState<GetUsers>({
-    limit: 0,
-    skip: 0,
-    total: 0,
-    users: [],
-  })
+  const {
+    data = { limit: 0, skip: 0, total: 0, users: [] },
+    error,
+    isLoading,
+  } = useSWR<GetUsers>("/users", userService.getUsers)
 
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<unknown>()
-
-  useEffect(() => {
-    ;(async () => {
-      try {
-        const data = await userService.getUsers()
-        setData(data)
-        setLoading(false)
-      } catch (error) {
-        setError(error)
-        setLoading(false)
-      }
-    })()
-  }, [])
-
-  return { users: data, loading, error }
+  return { users: data, isLoading, error }
 }
 
 export default useGetUsers

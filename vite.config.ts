f89e3c042c65d978627dad82d29c8ea9c0493b7b
Interceptor <darbaz.me@gmail.com>
2024-09-12 11:50:37 +0300

    Add Vitest configuration for testing in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { configDefaults } from "vitest/config"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-});
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "./src/setupTests.ts",
+    exclude: [...configDefaults.exclude],
+    coverage: {
+      reporter: ["text", "html"],
+    },
+  },
+})

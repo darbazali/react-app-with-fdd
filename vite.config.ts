ab71bd50130d5bd5e81c17c8f8aa0844ad60aa06
Interceptor <darbaz.me@gmail.com>
2024-09-12 11:51:35 +0300

    Update test setup file path and add jest-dom import
    - Changed setupFiles path in vite.config.ts to './src/app/setupTests.ts'
    - Added jest-dom import in the new setupTests.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "happy-dom",
-    setupFiles: "./src/setupTests.ts",
+    setupFiles: "./src/app/setupTests.ts",
     exclude: [...configDefaults.exclude],
     coverage: {
       reporter: ["text", "html"],

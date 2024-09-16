a276530ef2541052eb7d84c6a3c6e89951260aaa
Interceptor <darbaz.me@gmail.com>
2024-09-16 14:08:45 +0300

    Add conditional rendering to UserList component to handle empty user array

--- src/features/users/components/UserList.tsx
+++ src/features/users/components/UserList.tsx
@@ -6,12 +6,16 @@ interface Props {
   users: User[]
 }
 
-const UserList: React.FC<Props> = ({ users }) => (
-  <div>
-    {users.map((user) => (
-      <UserCard user={user} />
-    ))}
-  </div>
-)
+const UserList: React.FC<Props> = ({ users }) => {
+  if (users.length === 0) return null
+
+  return (
+    <div>
+      {users.map((user) => (
+        <UserCard user={user} />
+      ))}
+    </div>
+  )
+}
 
 export default UserList
